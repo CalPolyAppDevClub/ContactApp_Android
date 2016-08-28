8f5a649b9c6043bf4749256003037c203c8aafc8
Kris Campos <kcampos0101@gmail.com>
2016-08-27 21:26:57 -0700

    created flow from SodaActivity to DoneActivity

--- app/src/main/java/com/example/kris/contactapp/SodaActivity.java
+++ app/src/main/java/com/example/kris/contactapp/SodaActivity.java
@@ -1,7 +1,10 @@
 package com.example.kris.contactapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SodaActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class SodaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_soda);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Button btn = (Button) findViewById(R.id.btn_soda);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), DoneActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
