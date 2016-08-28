496fd0bc4df7b3b6d21d00cd20a75b05aea0ed26
Kris Campos <kcampos0101@gmail.com>
2016-08-27 21:13:45 -0700

    Updated last four fragments to be activities

---
+++ app/src/main/java/com/example/kris/contactapp/DoneActivity.java
@@ -0,0 +1,12 @@
+package com.example.kris.contactapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DoneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
