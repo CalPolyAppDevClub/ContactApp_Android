247cef13aef259af588b8d707fa079553fe0068d
Kris Campos <kcampos0101@gmail.com>
2016-08-27 21:22:06 -0700

    created flow from PizzaActivity to SodaActivity

--- app/src/main/java/com/example/kris/contactapp/PizzaActivity.java
+++ app/src/main/java/com/example/kris/contactapp/PizzaActivity.java
@@ -1,7 +1,10 @@
 package com.example.kris.contactapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PizzaActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class PizzaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pizza);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Button btn = (Button) findViewById(R.id.btn_pizza);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SodaActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
