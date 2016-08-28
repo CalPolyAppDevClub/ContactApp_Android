e692cc1e163c778f8d3a87c9f7914617864420bd
Kris Campos <kcampos0101@gmail.com>
2016-08-27 21:32:37 -0700

    created flow from DoneActivity to WelcomeActivity

--- app/src/main/java/com/example/kris/contactapp/DoneActivity.java
+++ app/src/main/java/com/example/kris/contactapp/DoneActivity.java
@@ -1,12 +1,36 @@
 package com.example.kris.contactapp;
 
+import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class DoneActivity extends AppCompatActivity {
 
+    private static final long TIME_ON_THIS_ACTIVITY = 3000; // 3 seconds
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_done);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // timer that goes back to Welcome screen upon completion.
+        new CountDownTimer(TIME_ON_THIS_ACTIVITY, TIME_ON_THIS_ACTIVITY) {
+            @Override
+            public void onTick(long l) {
+                // do not receive updates
+            }
+
+            @Override
+            public void onFinish() {
+                Intent intent = new Intent(getApplicationContext(), WelcomeActivity.class);
+                startActivity(intent);
+            }
+        }.start();
     }
 }
